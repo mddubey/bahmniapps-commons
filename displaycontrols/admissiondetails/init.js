192d3092afd3bb15e763d23563a72f9a6f6c33d3
MD Dubey <mddubey409@gmail.com>
2019-07-08 12:28:51 +0530

    Dubey | Move displaycontrols/admissiondetails as a library;

---
+++ displaycontrols/admissiondetails/init.js
@@ -0,0 +1,9 @@
+'use strict';
+window.Bahmni = Bahmni || {};
+Bahmni.Common = Bahmni.Common || {};
+Bahmni.Common.DisplayControl = Bahmni.Common.DisplayControl || {};
+Bahmni.Common.DisplayControl.AdmissionDetails = Bahmni.Common.DisplayControl.AdmissionDetails || {};
+
+angular.module('bahmni.common.displaycontrol.admissiondetails', []);
+
+require("./directives/admissionDetails")
