6658a228ed0fd0f9b5c16da4dae6779da675cfc6
MD Dubey <mddubey409@gmail.com>
2019-07-08 12:59:30 +0530

    Dubey | Move displaycontrols/navigationlinks as a library;

--- displaycontrols/init.js
+++ displaycontrols/init.js
@@ -5,9 +5,10 @@ Bahmni.Common.DisplayControl = Bahmni.Common.DisplayControl || {};
 
 angular.module('bahmni.common.displaycontrol', []);
 
-require("./patientprofile/init")
-require("./dashboard/init")
-require("./observation/init")
-require("./disposition/init")
-require("./admissiondetails/init")
-require("./diagnosis/init")
+require("./patientprofile/init");
+require("./dashboard/init");
+require("./observation/init");
+require("./disposition/init");
+require("./admissiondetails/init");
+require("./diagnosis/init");
+require("./navigationlinks/init");
---
+++ displaycontrols/navigationlinks/init.js
@@ -0,0 +1,9 @@
+'use strict';
+window.Bahmni = Bahmni || {};
+Bahmni.Common = Bahmni.Common || {};
+Bahmni.Common.DisplayControl = Bahmni.Common.DisplayControl || {};
+Bahmni.Common.DisplayControl.navigationLinks = Bahmni.Common.DisplayControl.navigationLinks || {};
+
+angular.module('bahmni.common.displaycontrol.navigationlinks', ['ui.router', 'ui.router.util']);
+
+require("./directives/navigationLinks")
